ca357e4fe5966f636f017a23d9117731c193d5a4
decourtney <53956789+decourtney@users.noreply.github.com>
2023-03-11 19:01:50 -0600

    mobile responsive

---
+++ README copy.md
@@ -0,0 +1,76 @@
+# TechNotesToday
+
+[![License: MIT License](https://img.shields.io/badge/License-MIT-blue.svg)](https://choosealicense.com/licenses/mit/)
+  
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Testing](#testing)
+* [Questions](#questions)
+* [Licensing](#licensing)
+
+## Description
+
+TechNotesToday is a basic forum for registered users to read, post, reply, and modify content.  
+
+Deployed Project: Not Currently Deployed
+
+<!-- <br>
+  <div>  
+      <img src="./assets/.png" target="_blank" alt="" style="max-width: 768px; display: block;" />  
+  </div>
+<br> -->
+
+## Installation
+
+To run Sequelized E-Commerce Back-end locally you'll need to first install Node and then install the following modules:
+
+- dotenv    v8.2.0
+- express   v4.17.1
+- mysql2    v2.1.0
+- sequelize v5.21.7
+- nodemon (recommended)
+
+- Insomnia for testing queries
+
+## Usage
+
+Create the database:
+- Navigate to the project's root folder, open a terminal, and log into mysql: **mysql -u root -p**
+- Then create the database using the db/schema.sql: **source db/schema.sql**
+
+Seed and start the database:
+- From another terminal seed the database by running the command: **node seeds/.**
+- Now start the server: **nodemon .**
+
+Once the server is running start Insomnia and reference the videos below to test queries:
+
+
+## Contribution
+
+Follow the "fork-and-pull" Git workflow.
+
+  1. **Fork** the repo on GitHub
+  2. **Clone** the project to your own machine
+  3. **Commit** changes to your own branch
+  4. **Push** your work back up to your fork
+  5. Submit a **Pull request** so that we can review your changes
+
+NOTE: Be sure to merge the latest from "upstream" before making a pull request!
+
+## Testing
+  
+N/A
+
+## Questions
+
+Feel free to contact me with any questions or comments:  
+<donovan.courtney@gmail.com>  
+<https://github.com/decourtney>
+
+## Licensing
+
+Code and Docs released under [MIT License](https://choosealicense.com/licenses/mit/).
--- controllers/api/forumRoutes.js
+++ controllers/api/forumRoutes.js
@@ -48,23 +48,27 @@ router.post('/', async (req, res) => {
     // Create handlebars template
     const mainContentTemplate = Handlebars.compile(`
     <section id="thread">
+
       <div id="mainPost" class="border-b-2 border-blue-200">
         <div id="postContentHeader">
-          <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-3xl py-5">{{mainPostContent.title}}</h2>
+
+          <div class="flex justify-between items-center mb-2">
+            <h2 class="font-semibold text-lg md:text-3xl leading-none">{{mainPostContent.title}}</h2>
+
             {{#compare mainPostContent.id currentUserID}}
             <div>
               <button id="main-post" type="button" data-postid={{mainPostContent.threadID}}
-                class="del-btn inline-block px-3 pb-1 bg-black text-blue-300 leading-tight uppercase rounded shadow-md hover:bg-blue-300 hover:text-slate-900 hover:shadow-lg active:outline-none active:ring-0 active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out">
-                  <span class="text-xs font-semibold pointer-events-none">Delete</span></button>           
+              class="del-btn flex align-middle px-2 bg-black text-blue-300 uppercase rounded shadow-md hover:bg-blue-300 hover:text-slate-900 hover:shadow-lg active:outline-none active:ring-0 active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out">
+                  <span class="text-xs font-semibold pointer-events-none">Del</span></button>           
             </div>
             {{/compare}}
           </div>
+
           <div class="flex items-center">
-            <span class="shrink-0"><img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-full w-10"
-                alt="Avatar">
+            <span class="shrink-0"><img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" 
+            class="rounded-full w-8 md:w10" alt="Avatar">
             </span>
-            <div class="px-2">
+            <div class="px-2 text-xs md:text-base">
               <p>Started by <span id="createdbyName" class="text-blue-600 font-medium">{{mainPostContent.username}}</span></p>
               <p>on <span id="createdbyDate" class="font-semibold">{{format_date mainPostContent.date_created}}</span></p>
             </div>
@@ -72,13 +76,15 @@ router.post('/', async (req, res) => {
         </div>
 
         <div id="postContent" data-postid={{mainPostContent.postid}}>
-          <div class="mt-4">
-            <p id="mainContent" class="content">
-              {{mainPostContent.content}}
+          <div class="mt-2 md:mt-4">
+            <p id="content" class="content">
+              <span class="text-sm md:text-base">{{mainPostContent.content}}</span>
 
               {{#compare mainPostContent.id currentUserID}}
-              <div class="flex justify-end">
-                <p class="edit-post text-xs font-semibold mr-2 cursor-pointer text-slate-400 hover:text-slate-700 active:text-slate-400 duration-150 ease-in-out"><span class=" pointer-events-none">EDIT</span></p>
+              <div class="flex justify-end mb-4">
+                <p class="edit-post text-xs font-semibold mr-2 cursor-pointer text-slate-400 hover:text-slate-700 active:text-slate-400 duration-150 ease-in-out">
+                  <span class=" pointer-events-none">edit</span>
+                </p>
               </div>
               {{/compare}}
 
@@ -86,55 +92,64 @@ router.post('/', async (req, res) => {
           </div>
         </div>
 
-        <div class="flex" id="postContentFooter">
-          <div class="flex pt-5 pb-10 mr-5">
+        <div id="postContentFooter" class="flex pt-2 md:pt-5 pb-2 md:pb-5">
+          <div class="flex md:pr-4">
             <button id="reply-btn" type="button" data-postid={{mainPostContent.threadID}} 
               class="reply-btn shrink-0 rounded-full p-1 mx-2 shadow-lg bg-blue-300 hover:bg-blue-400 active:shadow-none">
-              <span class="pointer-events-none"><img src="/images/reply_FILL0_wght400_GRAD0_opsz48.svg"
-                  class="rounded-full w-5 pointer-events-none" alt="Reply">
+              <span class="pointer-events-none">
+                <img src="/images/reply_FILL0_wght400_GRAD0_opsz48.svg"
+                class="rounded-full w-3 md:w-5 pointer-events-none" alt="Reply">
               </span>
             </button>
-            <p>Reply</p>
+            <p class="hidden md:flex">Reply</p>
           </div>
 
-          <div class="flex pt-5 pb-10 mr-5">
-            <span class="shrink-0 rounded-full p-1 mx-2 bg-gray-300"><img
-                src="/images/format_quote_FILL0_wght400_GRAD0_opsz48.svg" class="rounded-full w-5" alt="Quote">
-            </span>
-            <p>Quote</p>
+          <div class="flex">
+            <button id="reply-btn" type="button" data-postid={{mainPostContent.threadID}}
+              class="quote-btn pointer-events-none shrink-0 rounded-full p-1 mx-2 shadow-lg bg-gray-300 hover:bg-blue-400 active:shadow-none">
+              <span class="pointer-events-none">
+                <img src="/images/format_quote_FILL0_wght400_GRAD0_opsz48.svg" class="rounded-full w-3 md:w-5"
+                  alt="Quote">
+              </span>
+            </button>
+            <p class="hidden md:flex">Quote</p>
           </div>
         </div>     
       </div>
 
       {{#each posts}}  
-      <div id="postReply" class="my-5 border-b-2 border-blue-200">
+      <div id="postReply" class="my-3 border-b-2 pb-2 border-blue-200">
         <div class="flex items-center">
-          <span class="shrink-0"><img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-full w-10"
-            alt="Avatar">
+          <span class="shrink-0"><img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" 
+            class="rounded-full w-8 md:w10" alt="Avatar">
           </span>
+
           <div class="flex justify-between items-center w-full">
-            <div class="px-2">
+            <div class="px-2 text-xs md:text-base">
               <p><span class="text-blue-600 font-medium">{{user.username}}</span></p>
               <p>on <span class="font-semibold">{{../format_date date_created}}</span></p>
             </div>
+
             {{#compare user.id ../currentUserID}}
             <div>
               <button id="post" type="button" data-postid={{id}}
-                class="del-btn inline-block px-3 pb-1 bg-black text-blue-300 leading-tight uppercase rounded shadow-md hover:bg-blue-300 hover:text-slate-900 hover:shadow-lg active:outline-none active:ring-0 active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out">
-                  <span class="text-xs font-semibold pointer-events-none">Delete</span></button>
+                class="del-btn flex align-middle px-2 bg-black text-blue-300 uppercase rounded shadow-md hover:bg-blue-300 hover:text-slate-900 hover:shadow-lg active:outline-none active:ring-0 active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out">
+                <span class="text-xs font-semibold pointer-events-none">Del</span>
+              </button>
             </div>
             {{/compare}}
           </div>
         </div>
 
         <div id="postContent" data-postid={{id}}>
-          <div class="my-4">
-            <p class="content">
-              {{content}}
+          <div class="mt-2 md:mt-4 break-words overflow-hidden">
+            <p class="content" class="content">
+              <span class="text-sm md:text-base">{{content}}</span>
               
               {{#compare user.id ../currentUserID}}
-              <div class="flex justify-end mb-4">
-                <p class="edit-post text-xs font-semibold mr-2 cursor-pointer text-slate-400 hover:text-slate-700 active:text-slate-400 duration-150 ease-in-out"><span class=" pointer-events-none">EDIT</span></p>
+              <div class="flex">
+                <p class="edit-post w-full text-right text-xs font-semibold mr-2 cursor-pointer text-slate-400 hover:text-slate-700 active:text-slate-400 duration-150 ease-in-out">
+                <span class="pointer-events-none">edit</span></p>
               </div>
               {{/compare}}
             </p>
